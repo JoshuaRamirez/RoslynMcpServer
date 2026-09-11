3b7a8047ceb213899c4a3f4217839ca1fc7cd9b2
Joshua Ramirez <joshua.ramirez@me.com>
2026-09-11 23:29:04 +0000

    style: drop unused Microsoft.CodeAnalysis.Text usings after SpanCoversLine extract
    Copilot noted IDE0005 leftovers on Hierarchy/Extract ops once the type-local helpers were deleted.

--- src/RoslynMcp.Core/Refactoring/Extract/ExtractBaseClassOperation.cs
+++ src/RoslynMcp.Core/Refactoring/Extract/ExtractBaseClassOperation.cs
@@ -3,7 +3,6 @@ using System.Xml.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
 using RoslynMcp.Contracts.Enums;
 using RoslynMcp.Contracts.Errors;
 using RoslynMcp.Contracts.Models;
--- src/RoslynMcp.Core/Refactoring/Extract/ExtractInterfaceOperation.cs
+++ src/RoslynMcp.Core/Refactoring/Extract/ExtractInterfaceOperation.cs
@@ -1,7 +1,6 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
 using RoslynMcp.Contracts.Enums;
 using RoslynMcp.Contracts.Errors;
 using RoslynMcp.Contracts.Models;
