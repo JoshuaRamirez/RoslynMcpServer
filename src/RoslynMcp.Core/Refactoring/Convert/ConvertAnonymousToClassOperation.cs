3a32cd0d29a2395d02f48acea92fcc2805c497d9
Joshua Ramirez <joshua.ramirez@me.com>
2026-09-12 00:08:27 +0000

    fix: restore Microsoft.CodeAnalysis.Text usings in Convert ops
    ConvertAnonymous/ConvertTuple still use TextSpan; dropping the using broke the build (CS0246). Keep Text usings; blank-line cleanup on RenameFile remains.

--- src/RoslynMcp.Core/Refactoring/Convert/ConvertAnonymousToClassOperation.cs
+++ src/RoslynMcp.Core/Refactoring/Convert/ConvertAnonymousToClassOperation.cs
@@ -1,6 +1,7 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 using Microsoft.CodeAnalysis.FindSymbols;
 using RoslynMcp.Contracts.Enums;
 using RoslynMcp.Contracts.Errors;
