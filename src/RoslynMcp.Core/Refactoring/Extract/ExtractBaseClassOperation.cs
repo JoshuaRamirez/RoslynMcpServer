9ef759ec79697d4d0db16ddd14647a48880d1b82
Joshua Ramirez <joshua.ramirez@me.com>
2026-09-12 05:50:05 +0000

    fix: sort ExtractBaseClassOperation usings for Format Check (#1012)

--- src/RoslynMcp.Core/Refactoring/Extract/ExtractBaseClassOperation.cs
+++ src/RoslynMcp.Core/Refactoring/Extract/ExtractBaseClassOperation.cs
@@ -8,10 +8,10 @@ using RoslynMcp.Contracts.Errors;
 using RoslynMcp.Contracts.Models;
 using RoslynMcp.Core.FileSystem;
 using RoslynMcp.Core.Refactoring.Base;
-using RoslynMcp.Core.Refactoring.Utilities;
 using RoslynMcp.Core.Refactoring.Generate;
 using RoslynMcp.Core.Refactoring.Hierarchy;
 using RoslynMcp.Core.Refactoring.Rename;
+using RoslynMcp.Core.Refactoring.Utilities;
 using RoslynMcp.Core.Resolution;
 using RoslynMcp.Core.Workspace;
 
